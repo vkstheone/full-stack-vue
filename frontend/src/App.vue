4a09260e2891ebe57487e29b45ce82efcc3aac3a
vkstheone <89942870+vkstheone@users.noreply.github.com>
2023-11-05 13:46:29 -0300

    Primeiro commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+	<div id="app">
+		<Header />
+		<Menu />
+		<Content/>
+		<Footer/>
+	</div>
+</template>
+
+<script>
+import Header from '@/componetes/template/Header';
+import Menu from '@/componetes/template/Menu';
+import Content from '@/componetes/template/Content';
+import Footer from '@/componetes/template/Footer'
+
+
+export default {
+	name: "App",
+	components: { Header, Menu, Content, Footer }
+}
+</script>
+
+<style>
+*{
+	font-family:  "Lato", sans-serif;
+
+}
+body{
+	margin: 0;
+}
+
+#app{
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+
+	height: 100vh;
+	display: grid;
+	grid-template-rows: 60px 1fr 40px;
+	grid-template-columns: 300px 1fr;
+	grid-template-areas: 
+		"header header"
+		"menu content"
+		"menu footer"
+
+	;
+}
+
+</style>
---
+++ frontend/src/componetes/template/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+	<footer class="Footer">
+        <span>Copyright <strong>Vks</strong>2023-</span>
+    </footer>
+</template>
+
+<script>
+export default {
+	name: "Footer",
+}
+</script>
+
+<style>
+    .Footer{
+        grid-area: footer;
+        background-color: #ddd;
+        color: #333;
+
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding-right: 10px;
+    }
+</style>
